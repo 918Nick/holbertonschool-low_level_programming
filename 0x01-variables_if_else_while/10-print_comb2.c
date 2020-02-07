fa7bcfeb755ff848346d4ef772ad78739859bfb3
Nick McCullough <1714@holbertonschool.com>
2020-02-07 05:15:55 +0000

    updated version

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -6,21 +6,24 @@
  */
 int main(void)
 {
-int i;
-int j;
-for (i = '0'; i <= '9'; i++)
-{
-for(j = '0'; j <= '9'; j++)
-{
-putchar(i);
-putchar(j); 
- if (j == '9' && i == '9')
-{
-putchar('\n');
-}
-putchar(',');
-putchar(' ');
-}
-}
-return (0);
+  int i;
+  int j;
+  for (i = '0'; i <= '9'; i++)
+    {
+      for (j = '0'; j <= '9'; j++)
+	{
+	  putchar(i);
+	  putchar(j);
+	  if (j == '9' && i == '9')
+	    {
+	      putchar('\n');
+	    }
+	  else
+	    {
+	      putchar(',');
+	      putchar(' ');
+	    }
+	}
+    }
+  return (0);
 }
